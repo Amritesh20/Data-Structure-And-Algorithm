3bf19ba489e492725ec6945dbd9c4172c13ef257
Amritesh20 <amritesh20009@gmail.com>
2025-05-27 16:36:51 +0530

    Link List Insertion Beginning

---
+++ C/src/Link List Full.c
@@ -0,0 +1,52 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Insert at the beginning
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Insert at the end
+
+
+// Display the list
+void displayList(struct Node* head) {
+    struct Node* temp = head;
+    printf("Linked List: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Main function
+int main() {
+    struct Node* head = NULL;
+
+    insertAtBeginning(&head, 10);
+    displayList(head);     
+    insertAtBeginning(&head, 5);
+
+    displayList(head);
+
+    return 0;
+}
+
