226e7a6c651dd40a4def021e33ca85e8718f5aad
Amritesh20 <amritesh20009@gmail.com>
2025-06-22 07:35:27 +0530

    Some Common Sorting Algo

--- C/src/Some Common Sorting Algo.c
+++ C/src/Some Common Sorting Algo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
@@ -49,18 +50,16 @@ void merge(int arr[], int l, int m, int r) {
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    // Create temp arrays
     int *L = (int *)malloc(n1 * sizeof(int));
     int *R = (int *)malloc(n2 * sizeof(int));
 
     for (i = 0; i < n1; i++) L[i] = arr[l + i];
     for (j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
 
-    // Merge temp arrays back into arr[]
     i = 0, j = 0, k = l;
     while (i < n1 && j < n2)
         arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
-    
+
     while (i < n1) arr[k++] = L[i++];
     while (j < n2) arr[k++] = R[j++];
 
@@ -71,51 +70,46 @@ void merge(int arr[], int l, int m, int r) {
 void mergeSort(int arr[], int l, int r) {
     if (l < r) {
         int m = l + (r - l) / 2;
-
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
-
         merge(arr, l, m, r);
     }
 }
 
-// ----------------- Driver Code -----------------
-void runSorts(int arr[], int n) {
-    int temp[100];
-
-    printf("\nOriginal array:\n");
-    printArray(arr, n);
-
-    // Bubble Sort
+// ----------------- Timer and Driver -----------------
+void timeSort(void (*sortFunc)(int *, int), int arr[], int n, const char* sortName) {
+    int *temp = malloc(n * sizeof(int));
     memcpy(temp, arr, n * sizeof(int));
-    bubbleSort(temp, n);
-    printf("\nBubble Sort:\n");
-    printArray(temp, n);
 
-    // Selection Sort
-    memcpy(temp, arr, n * sizeof(int));
-    selectionSort(temp, n);
-    printf("\nSelection Sort:\n");
-    printArray(temp, n);
+    clock_t start = clock();
+    sortFunc(temp, n);
+    clock_t end = clock();
 
-    // Insertion Sort
-    memcpy(temp, arr, n * sizeof(int));
-    insertionSort(temp, n);
-    printf("\nInsertion Sort:\n");
-    printArray(temp, n);
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC * 1000000; // in microseconds
 
-    // Merge Sort
-    memcpy(temp, arr, n * sizeof(int));
-    mergeSort(temp, 0, n - 1);
-    printf("\nMerge Sort:\n");
+    printf("\n%s:\n", sortName);
     printArray(temp, n);
+    printf("Time taken: %.2f microseconds\n", time_taken);
+
+    free(temp);
+}
+
+// Wrapper for mergeSort (needs 3 args)
+void mergeSortWrapper(int *arr, int n) {
+    mergeSort(arr, 0, n - 1);
 }
 
 int main() {
-    int arr[] = {64, 25, 12, 22, 11};
+    int arr[] = {64, 25, 12, 22, 11, 55, 9, 77, 5, 99};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    runSorts(arr, n);
+    printf("Original Array:\n");
+    printArray(arr, n);
+
+    timeSort(bubbleSort, arr, n, "Bubble Sort");
+    timeSort(selectionSort, arr, n, "Selection Sort");
+    timeSort(insertionSort, arr, n, "Insertion Sort");
+    timeSort(mergeSortWrapper, arr, n, "Merge Sort");
 
     return 0;
 }
